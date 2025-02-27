c1883635a6026a919ce6365b9c7998ace7cf78fa
hanheel <hanhee630630@gmail.com>
2025-02-27 16:44:36 +0900

    feat : 엔터키 입력 구현

--- js/index.js
+++ js/index.js
@@ -15,40 +15,52 @@ console.log(
     "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
   "color: #d81b60; font-size: 16px; font-weight: bold;"
 );
+
 document.addEventListener("DOMContentLoaded", () => {
   const submitButton = document.querySelector("#submit");
   const cancelButton = document.querySelector("#cancel");
   const commentInput = document.querySelector("#comment-input");
 
   submitButton.addEventListener("click", () => {
-    const commentAuthor = document.querySelector(".comment-author");
-    const commentList = document.querySelector(".comment-list");
-
-    const commentText = commentInput.value.trim();
-    if (!commentText) return;
-
-    const listItem = document.createElement("li");
-
-    const commentItem = document.createElement("div");
-    commentItem.classList.add("comment-item");
-
-    const authorClone = commentAuthor.cloneNode(true);
-
-    const commentContent = document.createElement("div");
-    commentContent.classList.add("comment-content");
-    commentContent.textContent = commentText;
-
-    commentItem.appendChild(authorClone);
-    commentItem.appendChild(commentContent);
-    listItem.appendChild(commentItem);
-
-    commentList.appendChild(listItem);
-
-    commentInput.value = "";
+    addComment();
   });
 
   cancelButton.addEventListener("click", () => {
     commentInput.value = "";
   });
+  commentInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && !e.shiftKey && !e.isComposing) {
+      e.preventDefault();
+      addComment();
+    }
+  });
 });
-ㄴ;
+
+const addComment = () => {
+  const commentAuthor = document.querySelector(".comment-author");
+  const commentList = document.querySelector(".comment-list");
+  const commentInput = document.querySelector("#comment-input");
+
+  const commentText = commentInput.value.trim();
+  if (!commentText) return;
+
+  const listItem = document.createElement("li");
+
+  const commentItem = document.createElement("div");
+  commentItem.classList.add("comment-item");
+
+  const authorClone = commentAuthor.cloneNode(true);
+
+  const commentContent = document.createElement("div");
+  commentContent.classList.add("comment-content");
+  commentContent.textContent = commentText;
+
+  commentItem.appendChild(authorClone);
+  commentItem.appendChild(commentContent);
+  listItem.appendChild(commentItem);
+
+  commentList.appendChild(listItem);
+
+  commentInput.value = "";
+  commentInput.focus();
+};
